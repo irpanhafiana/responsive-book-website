8c63dc273138b524439f08e5ac34a235567be3ce
Irpan18 <irpanhafiana@gmail.com>
2024-04-03 12:18:58 +0700

    e-book landing page responsive

---
+++ assets/js/main.js
@@ -0,0 +1,203 @@
+/*=============== SEARCH ===============*/
+const searchBtn = document.getElementById("search-button"),
+  closeSearchBtn = document.getElementById("search-close"),
+  contentSearchBtn = document.getElementById("search-content");
+
+if (searchBtn) {
+  searchBtn.addEventListener("click", () => {
+    contentSearchBtn.classList.add("show-search");
+  });
+}
+
+if (closeSearchBtn) {
+  closeSearchBtn.addEventListener("click", () => {
+    contentSearchBtn.classList.remove("show-search");
+  });
+}
+
+/*=============== LOGIN ===============*/
+const loginBtn = document.getElementById("login-button"),
+  closeLoginBtn = document.getElementById("login-close"),
+  contentLoginBtn = document.getElementById("login-content");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    contentLoginBtn.classList.add("show-login");
+  });
+}
+
+if (closeLoginBtn) {
+  closeLoginBtn.addEventListener("click", () => {
+    contentLoginBtn.classList.remove("show-login");
+  });
+}
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+
+window.addEventListener("scroll", shadowHeader);
+
+/*=============== HOME SWIPER ===============*/
+
+let swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  spaceBetween: -24,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    1220: {
+      spaceBetween: -32,
+    },
+  },
+});
+/*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper(".featured__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
+/*=============== NEW SWIPER ===============*/
+let swiperNewBook = new Swiper(".new__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: "auto",
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+    },
+  },
+});
+/*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper(".testimonal__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
+/*=============== SHOW SCROLL UP ===============*/
+const scrollup = () => {
+  const scrollup = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollup.classList.add("show-buttom-scroll")
+    : scrollup.classList.remove("show-buttom-scroll");
+};
+
+window.addEventListener("scroll", scrollup);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .featured__container, .new__container, .join__data, .testimonial__container, .footer`
+);
+sr.reveal(`.home__images`, { delay: 600 });
+sr.reveal(`.services__card`, { interval: 100 });
+sr.reveal(`.discount__data`, { origin: "left" });
+sr.reveal(`.discount__images`, { origin: "right" });
+sr.reveal(`.discount__images`, { origin: "right" });
